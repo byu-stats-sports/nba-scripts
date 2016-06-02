0dead21da0fa4af709e627b17557dad8c9487db2
mwilliammyers <mwilliammyers@gmail.com>
2016-06-02 01:10:27 -0600

    Rewrite in pure SQL

---
+++ nba_never_injured.sql
@@ -0,0 +1,29 @@
+/*
+ * nba_never_injured.sql
+ *
+ * This script will perform a left join (all players - players who have been injured) 
+ * to determine the NBA players who have never been injured. 
+ *
+ * For each such player it reports:
+ *	{first} {last} {birthdate} {height} {weight} {position} {career games} {career minutes} {censor}
+ *
+ * Usage: 
+ *	mysql -u <user> -h <host> -p <database> < nba_never_injured.sql
+ */
+
+SELECT players.first, players.last, players.birthdate, players.ht, players.wt, players.pos, players.gp, players.mp, players.censor
+  FROM
+		--  all players
+		(SELECT first, last, abbr, birthdate, pos, ht, wt, COUNT(abbr) as gp, SUM(mp) as mp, censor
+			 FROM nbaGameInjuries 
+			 GROUP BY first, last, birthdate) players 
+	LEFT JOIN  --  all players - players who have been injured = players who have never been injured
+		--  players who have been injured at least once
+		(SELECT first, last, abbr, birthdate, pos, ht, wt, censor
+			 FROM nbaGameInjuries 
+			WHERE g_missed != 0 
+		  GROUP BY first, last, birthdate) injured
+	 ON players.abbr = injured.abbr
+WHERE injured.abbr IS NULL
+GROUP BY players.abbr
+ORDER BY players.last;
